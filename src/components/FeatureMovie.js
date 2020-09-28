10e32253f62cd7e922d43e2e2955e63ff9aa21d0
Marcelo <marcelo.trajano@outlook.com>
2020-09-28 11:54:47 -0300

    update

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -2,5 +2,16 @@ import React, { useState, useEffect } from "react";
 import "./FeatureMovie.css";
 
 export default ({ movie }) => {
-  return <div className="featureMovie">{movie.original_name}</div>;
+  return (
+    <section
+      className="featureMovie"
+      style={{
+        backgroudSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+      }}
+    >
+      <div>{movie.original_name}</div>
+    </section>
+  );
 };
